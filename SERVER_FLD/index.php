7d51e16fb5a9c264a9042cc4564b38afff9b7ce4
Ivan Simeonov <i8nov@outlook.com>
2022-10-30 13:53:40 +0200

    Making Server response header

--- SERVER_FLD/index.php
+++ SERVER_FLD/index.php
@@ -3,14 +3,25 @@
 require 'vendor/autoload.php';
 
 Flight::route('/', function(){
+    $timeNow = date('H:i:s');
+    $dateNow = date('Y-m-d');
 
-    $timeNow = date('Y-m-d H:i:s');
-
-
-    echo 'hello world!';
-    echo $timeNow;
+    $response  = array(
+        'http_code' => 200,
+        'state' => 'UP',
+        'date' => $dateNow,
+        'time' => $timeNow
+    );
+    Flight::json($response);
 });
 
+Flight::map('error', function(Exception $ex){
+    Flight::redirect('/');
+});
+
+Flight::map('notFound', function(){
+    Flight::redirect('/');
+});
 
 Flight::start();
 
